c2034a8fa5321208914db2c23b7b9d7bc105adb7
nutella-ops <52546474+nutella-ops@users.noreply.github.com>
2020-01-28 17:01:05 -0800

    Add files via upload
    tokenizes user input and generates a text file with every possible combination, with optional case sensitivity.

---
+++ passwd_dict-stable_case-mirror.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+import string
+
+## user input
+usr_in = input("elements to combine: ")
+
+## print unique occurences from user input
+#tokenized user input is independent variable of function
+def unique(usr_in):
+    #create a new empty list
+    single_char = []
+    #iterate thru tokenized user input 
+    for val in usr_in: 
+        #if tokenized char not in empty list...
+        if val not in single_char:
+            #...append to list 
+            single_char.append(val) 
+            #else skip char
+
+    return single_char 
+    print(unique(usr_in))
+
+## mirror opposite case into list
+def case(usr_in):
+    #tokenize user input
+    usrin = list(usr_in)
+    #uppercase copy of user input
+    usrup = list(usr_in.upper())
+    #lowercase copy of user input
+    usrdn = list(usr_in.lower())
+    #concatenate user input
+    case = usrin + usrup + usrdn
+    #create a new empty list
+    single_char = []
+    #iterate thru tokenized user input 
+    for val in case: 
+       #if tokenized char not in empty list...
+        if val not in single_char:
+            #...append to list 
+            single_char.append(val) 
+            #else skip char
+            
+    return single_char 
+
+print(unique(usr_in))
+print(case(usr_in))
